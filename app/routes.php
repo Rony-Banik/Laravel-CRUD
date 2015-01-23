b62730531a58d969d9de97f90b59d1c25c7c1e63
ROny-Banik <arko.surjobd@gmail.com>
2015-01-23 08:02:05 +0600

    Creating Sample Users Table and Preparing Route
    Some sample experiment by creating users table

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,7 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+Route::get('/test/create', 'UsersController@getCreate');
+Route::controller('test','UsersController');
+
+//Route::controller(uri, controller, names)
